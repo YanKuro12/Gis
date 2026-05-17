e762e49efdb1df6f5698df4f4c115a8c359f39e8
YanKuro12 <kurobithub@gmail.com>
2026-05-18 00:23:50 +0700

    Update PythonRunner.kt

--- app/src/main/java/com/kurox/qwerty/PythonRunner.kt
+++ app/src/main/java/com/kurox/qwerty/PythonRunner.kt
@@ -1,9 +1,11 @@
 package com.kurox.qwerty
 
-object PythonRunner {
-
-    fun run(code: String): String {
-
-        return "Running Python:\n$code"
+fun run(code: String): String {
+    return try {
+        val py = Python.getInstance()
+        val result = py.getModule("__main__").callAttr("exec", code)
+        "Python Execution Result:\n$result"
+    } catch (e: Exception) {
+        "Python Error: ${e.message}"
     }
 }
